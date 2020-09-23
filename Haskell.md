1ec104df4e30f92789264f553e1a31f36e91bc4d
Benjamin Kovach <bkovach13@gmail.com>
2020-09-23 10:43:58 -0400

    Play with folgezetten links

--- Haskell.md
+++ Haskell.md
@@ -2,3 +2,8 @@
 date: 2020-09-22T10:59
 title: Haskell
 ---
+
+## See also
+
+- [[[Monoid]]]
+- [[[Sum Type]]]
--- fixup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-
-mv 1b112336.md "Smart Constructor.md"
-find ./ -name "*.md" -exec sed -i -e 's/1b112336/Smart Constructor/g' {} \;
