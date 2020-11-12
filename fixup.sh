9944b7920cd1a3a1647ff9024e71b39ab29794f9
Benjamin Kovach <bkovach13@gmail.com>
2020-11-12 11:04:35 -0500

    Remove fixup file

--- fixup.sh
+++
@@ -1,2 +0,0 @@
-find ./ -name "*.md" -exec sed -i -e 's/https:\/\/ben-kovach-blog-assets.s3.amazonaws.comhttps:\/\/ben-kovach-blog-assets.s3.amazonaws.com\/images\/\/\//https:\/\/ben-kovach-blog-assets.s3.amazonaws.com\/images\//g' {} \;
-
